9c8bb5638e43bf8b298a0e52fc7979a7d07d27a2
Hamish Mackenzie <hamish.k.mackenzie@googlemail.com>
2010-08-21 15:32:06 +0200

    Bump version to 0.8.1.0
    Ignore-this: b2d69176c775fb7840b3c82c11614d24
    
    darcs-hash:20100821133206-47c18-b48205e0b83f2633915399371b49f24c70102a5e.gz

--- src/IDE/Core/CTypes.hs
+++ src/IDE/Core/CTypes.hs
@@ -89,7 +89,7 @@ import qualified Distribution.InstalledPackageInfo as IPI
 --
 
 leksahVersion, configDirName :: String
-leksahVersion = "0.8"
+leksahVersion = "0.8.1"
 configDirName = ".leksah-" ++ leksahVersion
 
 metadataVersion :: Integer
