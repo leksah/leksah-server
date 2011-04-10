78c40bbe413b2ed43e5177faf14c829054cbd673
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2011-04-11 06:21:50 +1200

    Set leksahVersion to "0.10" and leksah-server to 0.10.0

--- src/IDE/Core/CTypes.hs
+++ src/IDE/Core/CTypes.hs
@@ -89,7 +89,7 @@ import qualified Distribution.InstalledPackageInfo as IPI
 --
 
 leksahVersion, configDirName :: String
-leksahVersion = "0.9"
+leksahVersion = "0.10"
 configDirName = ".leksah-" ++ leksahVersion
 
 metadataVersion :: Integer
